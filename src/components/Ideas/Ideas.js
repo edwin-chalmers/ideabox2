80b869f53b3c1276664e3c308cbf0e1f902a661e
Edwin Chalmers <edwinchalmers@gmail.com>
2024-03-18 14:17:16 -0600

    inital commit

---
+++ src/components/Ideas/Ideas.js
@@ -0,0 +1,12 @@
+import { useState } from "react"
+import "./Ideas.css"
+
+function Ideas() {
+    return (
+        <div>
+            <h2>Ideas go here!</h2>
+        </div>
+    )
+}
+
+export default Ideas
